b19a6b2f522488993fe0532bbde2a30f5694ac98
selession21st <waiganjokibet001@gmail.com>
2023-11-28 12:15:27 -0800

    print last digit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@
 def fizzbuzz():
     for numbers in range(1, 101):
         if numbers % 3 == 0 and numbers % 5 == 0:
-            print("Fizzbuzz ", end="")
+            print("FizzBuzz ", end="")
         elif numbers % 3 == 0:
             print("Fizz ", end="")
         elif numbers % 5 == 0:
