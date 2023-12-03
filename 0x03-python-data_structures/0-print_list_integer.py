e91885c32e3f7e552d471a58e7ca744fd04ab5a8
selession21st <waiganjokibet001@gmail.com>
2023-12-03 12:18:15 -0800

    retrives element

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 '''function that prints all integers of a list.'''
+
 def print_list_integer(my_list=[]):
     for x in range(len(my_list)):
         print("{:d}".format(my_list[x]))
