116b1541766eec372c60c78f1ca83480efa528d2
selession21st <waiganjokibet001@gmail.com>
2023-12-19 08:39:13 -0800

    documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-''' an empty class Square that defines a square'''
+'''an empty class Square that defines a square'''
 
 
 class Square:
+    '''represents a square'''
     pass
