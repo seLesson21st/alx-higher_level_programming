084d0d6295ba0aec8a11eec47b1c56d5f531fe40
selession21st <waiganjokibet001@gmail.com>
2023-12-19 08:07:10 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+''' defines a square by: (based on 0-square.py)'''
+
+class Square:
+    '''represents a square'''
+
+    def __init__(self, size):
+        '''initializes a square
+        Args:
+            '''
+        self.__size = size
