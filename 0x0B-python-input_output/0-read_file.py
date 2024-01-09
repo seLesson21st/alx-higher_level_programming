cad7db84f4d5eb9000b56eefa7e38da019d87848
selession21st <waiganjokibet001@gmail.com>
2024-01-09 08:24:00 -0800

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Defines a text file reading function'''
+
+
+def read_file(filename=""):
+    '''function that reads a text file (UTF8) and prints it to stdout'''
+    with open(filename, encoding="utf_8") as f:
+        print(f.read(), end="")
